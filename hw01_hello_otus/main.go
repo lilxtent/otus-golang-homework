e97c01bbc94bf101ba8e8041682e510e2e3b42a1
Александр Ходкевич <aleksandr@MacBook-Pro-Aleksandr.local>
2026-01-31 01:29:19 +0300

    hw01 + home work

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+
+	"golang.org/x/example/hello/reverse"
+)
+
 func main() {
-	// Place your code here.
+	reversedMessage := reverse.String("Hello, OTUS!")
+	fmt.Println(reversedMessage)
 }
