ffb557820dc95136c5a255378d886acdeb07eed7
Aleksandr Khodkevich <lilxtent@gmail.com>
2026-03-27 22:47:54 +0300

    inital test passed

--- hw02_unpack_string/CursorState.go
+++ hw02_unpack_string/CursorState.go
@@ -1,24 +1,62 @@
 package hw02unpackstring
 
 type CursorState struct {
-	LastLetter   *rune
-	RepeatTimes  int
-	EscapeNext   bool
-	ReadyToWrite bool
+	sequence    *[]rune
+	repeatTimes *int
+	escape      *bool
 }
 
 func NewCursorState() *CursorState {
 	return &CursorState{
-		LastLetter:   nil,
-		RepeatTimes:  -1,
-		EscapeNext:   false,
-		ReadyToWrite: false,
+		sequence:    nil,
+		repeatTimes: nil,
+		escape:      nil,
 	}
 }
 
+func (cursorState *CursorState) SetSequence(runes ...rune) {
+	cursorState.sequence = &runes
+}
+
+func (cursorState *CursorState) GetSequence() *[]rune {
+	return cursorState.sequence
+}
+
+func (cursorState *CursorState) IsSequenceSpecified() bool {
+	return cursorState.sequence != nil
+}
+
 func (cursorState *CursorState) Reset() {
-	cursorState.LastLetter = nil
-	cursorState.RepeatTimes = -1
-	cursorState.EscapeNext = false
-	cursorState.ReadyToWrite = false
+	cursorState.sequence = nil
+	cursorState.repeatTimes = nil
+	cursorState.escape = nil
+}
+
+func (cursorState *CursorState) ReadyToFlush() bool {
+	return cursorState.sequence != nil && cursorState.repeatTimes != nil
+}
+
+func (cursorState *CursorState) Escaped() bool {
+	return cursorState.escape != nil && *cursorState.escape
+}
+
+func (cursorState *CursorState) Escape() {
+	escape := true
+	cursorState.escape = &escape
+}
+
+func (cursorState *CursorState) SetRepeatTimes(repeatTimes int) {
+	cursorState.repeatTimes = &repeatTimes
+}
+
+func (cursorState *CursorState) IsRepeatTimesSpecified() bool {
+	return cursorState.repeatTimes != nil
+}
+
+func (cursorState *CursorState) GetRepeatTimes() int {
+	if cursorState.repeatTimes == nil {
+		return 1
+	}
+
+	return *cursorState.repeatTimes
 }
--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,7 +2,6 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"strconv"
 	"strings"
 	"unicode"
 )
@@ -10,88 +9,91 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(input string) (string, error) {
-	resultString := strings.Builder{}
-	cursorState := NewCursorState()
-	index := 0
-
 	if input == "" {
 		return "", nil
 	}
 
-	if unicode.IsDigit(rune(input[0])) {
-		return "", ErrInvalidString
-	}
+	cursor := CursorState{}
+	stringBuilder := strings.Builder{}
 
-	for {
-		if index >= len(input) {
-			if cursorState.EscapeNext {
+	for _, runeElement := range input {
+		if IsSequenceEnded(&cursor, runeElement) {
+			cursor.SetRepeatTimes(1)
+		}
+
+		if cursor.ReadyToFlush() {
+			err := Flush(&stringBuilder, &cursor)
+			if err != nil {
+				return "", err
+			}
+		}
+
+		if runeElement == '\\' {
+			if cursor.Escaped() {
 				return "", ErrInvalidString
 			}
 
-			stringToWrite := CreateString(*cursorState.LastLetter, 1)
-			resultString.WriteString(stringToWrite)
-			cursorState.Reset()
-
-			break
-		}
-
-		if cursorState.ReadyToWrite {
-			stringToWrite := CreateString(*cursorState.LastLetter, cursorState.RepeatTimes)
-			resultString.WriteString(stringToWrite)
-			cursorState.Reset()
-		}
-
-		currentSymbol := rune(input[index])
-
-		if unicode.IsLetter(currentSymbol) || index >= len(input) {
-			if cursorState.LastLetter != nil {
-				cursorState.RepeatTimes = 1
-				cursorState.ReadyToWrite = true
-
-				continue
+			cursor.Escape()
+		} else if unicode.IsDigit(runeElement) {
+			if cursor.IsRepeatTimesSpecified() {
+				return "", ErrInvalidString
 			}
 
-			cursorState.LastLetter = &currentSymbol
-
-			index++
-			continue
-		}
-
-		if unicode.IsDigit(currentSymbol) {
-			if cursorState.EscapeNext {
-				cursorState.LastLetter = &currentSymbol
+			if cursor.Escaped() {
+				cursor.SetSequence(runeElement)
 			} else {
-				digit, atoiErr := strconv.Atoi(string(currentSymbol))
-
-				if atoiErr != nil {
-					return "", ErrInvalidString
-				}
-
-				cursorState.RepeatTimes = digit
-				cursorState.ReadyToWrite = true
+				cursor.SetRepeatTimes(int(runeElement - '0'))
 			}
-
-			index++
-			continue
+		} else {
+			cursor.SetSequence(runeElement)
 		}
-
-		if currentSymbol == '\\' {
-			cursorState.EscapeNext = true
-
-			index++
-			continue
-		}
-
-		cursorState.ReadyToWrite = true
 	}
 
-	return resultString.String(), nil
+	if cursor.IsSequenceSpecified() && !cursor.IsRepeatTimesSpecified() {
+		cursor.SetRepeatTimes(1)
+	}
+
+	if cursor.ReadyToFlush() {
+		err := Flush(&stringBuilder, &cursor)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	if cursor.IsSequenceSpecified() || cursor.IsRepeatTimesSpecified() || cursor.Escaped() {
+		return "", ErrInvalidString
+	}
+
+	return stringBuilder.String(), nil
 }
 
-func CreateString(symbol rune, repeat int) string {
-	if repeat == 1 {
-		return string(symbol)
+func Flush(stringsBuilder *strings.Builder, cursorState *CursorState) error {
+	if cursorState == nil {
+		return errors.New("cursorState state cannot be nil")
 	}
 
-	return strings.Repeat(string(symbol), repeat)
+	if !cursorState.IsSequenceSpecified() {
+		return errors.New("cursorState.Sequence must be specified")
+	}
+
+	stringToWrite := GetStringToWrite(cursorState)
+	_, err := stringsBuilder.WriteString(*stringToWrite)
+	cursorState.Reset()
+
+	return err
+}
+
+func GetStringToWrite(cursorState *CursorState) *string {
+	sequenceAsString := string(*cursorState.GetSequence())
+
+	if cursorState.IsRepeatTimesSpecified() {
+		repeatedString := strings.Repeat(sequenceAsString, cursorState.GetRepeatTimes())
+		return &repeatedString
+	} else {
+		return &sequenceAsString
+	}
+}
+
+func IsSequenceEnded(cursor *CursorState, runeElement rune) bool {
+	return cursor.IsSequenceSpecified() && !cursor.IsRepeatTimesSpecified() && runeElement != '\\' && !unicode.IsDigit(runeElement)
 }
