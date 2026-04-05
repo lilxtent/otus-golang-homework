8832f01b1d6eea3ffc76928838b3823e40199331
Aleksandr Khodkevich <lilxtent@gmail.com>
2026-04-05 21:21:03 +0300

    put each test case into its own test method to fit under 60 lines

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -9,120 +9,116 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCache(t *testing.T) {
-	t.Run("empty cache", func(t *testing.T) {
-		c := NewCache(10)
+func TestCacheEmptyCache(t *testing.T) {
+	c := NewCache(10)
 
-		_, ok := c.Get("aaa")
-		require.False(t, ok)
+	_, ok := c.Get("aaa")
+	require.False(t, ok)
 
-		_, ok = c.Get("bbb")
-		require.False(t, ok)
-	})
+	_, ok = c.Get("bbb")
+	require.False(t, ok)
+}
 
-	t.Run("simple", func(t *testing.T) {
-		c := NewCache(5)
+func TestCacheSimple(t *testing.T) {
+	c := NewCache(5)
 
-		wasInCache := c.Set("aaa", 100)
-		require.False(t, wasInCache)
+	wasInCache := c.Set("aaa", 100)
+	require.False(t, wasInCache)
 
-		wasInCache = c.Set("bbb", 200)
-		require.False(t, wasInCache)
+	wasInCache = c.Set("bbb", 200)
+	require.False(t, wasInCache)
 
-		val, ok := c.Get("aaa")
-		require.True(t, ok)
-		require.Equal(t, 100, val)
+	val, ok := c.Get("aaa")
+	require.True(t, ok)
+	require.Equal(t, 100, val)
 
-		val, ok = c.Get("bbb")
-		require.True(t, ok)
-		require.Equal(t, 200, val)
+	val, ok = c.Get("bbb")
+	require.True(t, ok)
+	require.Equal(t, 200, val)
 
-		wasInCache = c.Set("aaa", 300)
-		require.True(t, wasInCache)
+	wasInCache = c.Set("aaa", 300)
+	require.True(t, wasInCache)
 
-		val, ok = c.Get("aaa")
-		require.True(t, ok)
-		require.Equal(t, 300, val)
+	val, ok = c.Get("aaa")
+	require.True(t, ok)
+	require.Equal(t, 300, val)
 
-		val, ok = c.Get("ccc")
-		require.False(t, ok)
-		require.Nil(t, val)
-	})
+	val, ok = c.Get("ccc")
+	require.False(t, ok)
+	require.Nil(t, val)
+}
 
-	t.Run("purge logic", func(t *testing.T) {
-		cache := NewCache(2)
+func TestCachePurgeLogic(t *testing.T) {
+	cache := NewCache(2)
 
-		cache.Set("a", 1)
-		cache.Set("b", 2)
+	cache.Set("a", 1)
+	cache.Set("b", 2)
 
-		cache.Clear()
+	cache.Clear()
 
-		val, ok := cache.Get("a")
-		require.False(t, ok)
-		require.Nil(t, val)
+	val, ok := cache.Get("a")
+	require.False(t, ok)
+	require.Nil(t, val)
 
-		val, ok = cache.Get("b")
-		require.False(t, ok)
-		require.Nil(t, val)
+	val, ok = cache.Get("b")
+	require.False(t, ok)
+	require.Nil(t, val)
 
-		cache.Set("c", 3)
+	cache.Set("c", 3)
 
-		val, ok = cache.Get("c")
-		require.True(t, ok)
-		require.Equal(t, 3, val)
-	})
+	val, ok = cache.Get("c")
+	require.True(t, ok)
+	require.Equal(t, 3, val)
+}
 
-	t.Run("zero capacity", func(t *testing.T) {
-		cache := NewCache(0)
+func TestCacheZeroCapacity(t *testing.T) {
+	cache := NewCache(0)
 
-		cache.Set("a", 1)
+	cache.Set("a", 1)
 
-		val, ok := cache.Get("a")
+	val, ok := cache.Get("a")
+	require.False(t, ok)
+	require.Nil(t, val)
+}
 
-		require.False(t, ok)
-		require.Nil(t, val)
-	})
+func TestCacheExistedValue(t *testing.T) {
+	cache := NewCache(3)
 
-	t.Run("existed value", func(t *testing.T) {
-		cache := NewCache(3)
+	alreadyExisted := cache.Set("a", 1)
+	require.False(t, alreadyExisted)
 
-		alreadyExisted := cache.Set("a", 1)
-		require.False(t, alreadyExisted)
+	alreadyExisted = cache.Set("a", 2)
+	require.True(t, alreadyExisted)
+}
 
-		alreadyExisted = cache.Set("a", 2)
-		require.True(t, alreadyExisted)
-	})
+func TestCacheSetSameValue(t *testing.T) {
+	cache := NewCache(3)
 
-	t.Run("set same value", func(t *testing.T) {
-		cache := NewCache(3)
+	cache.Set("a", 1)
+	cache.Set("a", 2)
 
-		cache.Set("a", 1)
-		cache.Set("a", 2)
+	value, ok := cache.Get("a")
+	require.True(t, ok)
+	require.Equal(t, 2, value)
+}
 
-		value, ok := cache.Get("a")
+func TestCacheCapacityOverflow(t *testing.T) {
+	cache := NewCache(2)
 
-		require.True(t, ok)
-		require.Equal(t, 2, value)
-	})
+	cache.Set("a", 1)
+	cache.Set("b", 2)
+	cache.Set("c", 3)
 
-	t.Run("capacity overflow", func(t *testing.T) {
-		cache := NewCache(2)
+	_, ok := cache.Get("a")
+	require.False(t, ok)
 
-		cache.Set("a", 1)
-		cache.Set("b", 2)
-		cache.Set("c", 3)
+	value, ok := cache.Get("b")
+	require.True(t, ok)
+	require.Equal(t, 2, value)
 
-		_, ok := cache.Get("a")
-		require.False(t, ok)
-
-		value, ok := cache.Get("b")
-		require.True(t, ok)
-		require.Equal(t, 2, value)
-
-		value, ok = cache.Get("c")
-		require.True(t, ok)
-		require.Equal(t, 3, value)
-	})
+	value, ok = cache.Get("c")
+	require.True(t, ok)
+	require.Equal(t, 3, value)
 }
 
 func TestCacheMultithreading(_ *testing.T) {
