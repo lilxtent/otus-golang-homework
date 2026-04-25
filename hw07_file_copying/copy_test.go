7ebee52173f97aa8573bd056dde379b65c47d6d0
Aleksandr Khodkevich <lilxtent@gmail.com>
2026-04-26 02:44:55 +0300

    Add tests from test.sh

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -35,3 +35,67 @@ func TestCopy(t *testing.T) {
 		require.Equal(t, data, out)
 	})
 }
+
+func TestCopyShouldBeSucces(t *testing.T) {
+	testCases := []struct {
+		name         string
+		offset       int64
+		limit        int64
+		expectedPath string
+	}{
+		{
+			name:         "offset0_limit0",
+			offset:       0,
+			limit:        0,
+			expectedPath: "testdata/out_offset0_limit0.txt",
+		},
+		{
+			name:         "offset0_limit10",
+			offset:       0,
+			limit:        10,
+			expectedPath: "testdata/out_offset0_limit10.txt",
+		},
+		{
+			name:         "offset0_limit1000",
+			offset:       0,
+			limit:        1000,
+			expectedPath: "testdata/out_offset0_limit1000.txt",
+		},
+		{
+			name:         "offset0_limit10000",
+			offset:       0,
+			limit:        10000,
+			expectedPath: "testdata/out_offset0_limit10000.txt",
+		},
+		{
+			name:         "offset100_limit1000",
+			offset:       100,
+			limit:        1000,
+			expectedPath: "testdata/out_offset100_limit1000.txt",
+		},
+		{
+			name:         "offset6000_limit1000",
+			offset:       6000,
+			limit:        1000,
+			expectedPath: "testdata/out_offset6000_limit1000.txt",
+		},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(testCase.name, func(t *testing.T) {
+			tmpDir := t.TempDir()
+			outPath := filepath.Join(tmpDir, "out.txt")
+
+			err := Copy("testdata/input.txt", outPath, testCase.offset, testCase.limit)
+			require.NoError(t, err)
+
+			got, err := os.ReadFile(outPath)
+			require.NoError(t, err)
+
+			resultCopyFileContent, err := os.ReadFile(testCase.expectedPath)
+			require.NoError(t, err)
+
+			require.Equal(t, resultCopyFileContent, got)
+		})
+	}
+}
