8e5ccebb54242869aa142c142f737bd17460b8c5
Aleksandr Khodkevich <lilxtent@gmail.com>
2026-04-26 01:01:29 +0300

    Offset must be smaller then file size test

--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,17 @@
 package main
 
-import "testing"
+import (
+	"math"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("Offset must be smaller then file size", func(t *testing.T) {
+		maxInt64Value := math.MaxInt64
+		err := Copy("testdata/input.txt", "out_test_tmp.txt", int64(maxInt64Value), 0)
+
+		require.Error(t, ErrOffsetExceedsFileSize, err)
+	})
 }
