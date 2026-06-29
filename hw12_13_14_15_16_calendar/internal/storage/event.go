b72c9a8e9fe3b75b127b8c7d0135a460e24fd0fc
Aleksandr Khodkevich <lilxtent@gmail.com>
2026-06-29 22:27:43 +0300

    hw12: add models

--- hw12_13_14_15_16_calendar/internal/storage/event.go
+++ hw12_13_14_15_16_calendar/internal/storage/event.go
@@ -1,7 +1,24 @@
 package storage
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type Event struct {
-	ID    string
-	Title string
-	// TODO
+	ID           uuid.UUID
+	Title        string
+	Date         time.Time
+	Duration     time.Duration
+	Description  *string
+	UserID       uuid.UUID
+	NotifyBefore *time.Duration
+}
+
+type Notification struct {
+	EventID    string
+	Title      string
+	Date       time.Time
+	UserToSend uuid.UUID
 }
