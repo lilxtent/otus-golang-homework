666866307227f01c593ddfa44601bd4245980c5a
Aleksandr Khodkevich <lilxtent@gmail.com>
2026-06-30 00:10:52 +0300

    hw12: add common interface for storage

---
+++ hw12_13_14_15_16_calendar/internal/storage/storage.go
@@ -0,0 +1,16 @@
+package storage
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Storage interface {
+	CreateEvent(event Event) error
+	UpdateEvent(id uuid.UUID, event Event) error
+	DeleteEvent(id uuid.UUID) error
+	ListEventsForDay(date time.Time) ([]Event, error)
+	ListEventsForWeek(startOfWeek time.Time) ([]Event, error)
+	ListEventsForMonth(startOfMonth time.Time) ([]Event, error)
+}
